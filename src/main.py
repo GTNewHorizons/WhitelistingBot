3b046d25ef486ada5a3def220e798ed4e5bfeca5
boubou19 <miisterunknown@gmail.com>
2025-07-20 17:03:34 +0200

    revert better imports

--- src/main.py
+++ src/main.py
@@ -13,8 +13,8 @@ import requests
 from discord import Member, TextChannel, User
 from discord.ext.commands import Bot
 
-from command_cog import CommandsCog
-from question import Question, QuestionType
+from src.command_cog import CommandsCog
+from src.question import Question, QuestionType
 
 logging.basicConfig(filename=Path(__file__).parent / "bot.log", filemode="a", format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", level=logging.INFO)
 
